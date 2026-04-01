34fd2c0bd7e0ad0b2af59578e8c36aacfac77789
Tyler JB <tylerscv22@gmail.com>
2026-04-01 05:51:42 -0700

    Enable wagmi support for Abstract and Base chains

--- web/src/lib/wagmi.ts
+++ web/src/lib/wagmi.ts
@@ -1,9 +1,10 @@
 import { createConfig, http } from "wagmi";
-import { abstract } from "wagmi/chains";
+import { abstract, base } from "wagmi/chains";
 
 export const config = createConfig({
-  chains: [abstract],
+  chains: [abstract, base],
   transports: {
     [abstract.id]: http("https://api.mainnet.abs.xyz"),
+    [base.id]: http("https://mainnet.base.org"),
   },
 });
